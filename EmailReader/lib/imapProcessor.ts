a29f13df45f1daeab1415b470f492f7205378d8e
Nick <68756004+RGNGIT@users.noreply.github.com>
2022-05-12 11:35:07 +0500

    Update imapProcessor.ts

--- EmailReader/lib/imapProcessor.ts
+++ EmailReader/lib/imapProcessor.ts
@@ -1,7 +1,7 @@
 import emailGetter from "./emailGetter";
 
 const serverCheck = (Imap) => {
-    Imap.openBox('INBOX', true, (err, box) => {
+    Imap.openBox('INBOX', false, (err, box) => {
         if (err) throw err;
         console.log("Server is ready! Getting the emails...");
         emailGetter(Imap);
