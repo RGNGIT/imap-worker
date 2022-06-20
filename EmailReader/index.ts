5067248b5468a1bf6e5f82529fc7837405ca781f
Nikita Gnezdilov <vnifedow@bk.ru>
2022-06-20 14:12:28 +0500

    ++

--- EmailReader/index.ts
+++ EmailReader/index.ts
@@ -6,31 +6,6 @@ import config from './config';
 const {misc: {cronTime}} = config;
 let isJobRunning = false;
 
-const start = () => {
-    if (isJobRunning) {
-        console.log('Previous job is currently running...');
-        return;
-    } else {
-        isJobRunning = true;
-    }
-    const imap = ImapInstanse;
-    imap.once('ready', () => {
-        ImapProcessor(imap);
-        console.log("Successfully set up IMAP!");
-    });
-    imap.once('error', (err) => {
-        console.log(`Server Error: ${err}`);
-        isJobRunning = false;
-    });
-    imap.once('end', () => {
-        console.log('Connection terminated.');
-        isJobRunning = false;
-    });
-    imap.connect();
-}
-
-start();
-/*
 const job = new CronJob(cronTime, () => {
     if (isJobRunning) {
         console.log('Previous job is currently running...');
@@ -55,4 +30,3 @@ const job = new CronJob(cronTime, () => {
 });
 
 job.start();
-*/
