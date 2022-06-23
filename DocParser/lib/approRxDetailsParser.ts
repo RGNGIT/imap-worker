4f726232b29b883cfbd53f3831ff5a1662caacc0
Nikita Gnezdilov <vnifedow@bk.ru>
2022-06-24 03:49:41 +0500

    +

--- DocParser/lib/approRxDetailsParser.ts
+++ DocParser/lib/approRxDetailsParser.ts
@@ -3,37 +3,69 @@ import fs from 'fs';
 import config from '../config';
 import s3 from '../s3';
 
-const {
-    misc: {
+const {misc: {
         tempFolder
-    }
-}= config;
+    }} = config;
 
 class ApproRxDetailsParser {
 
-    fileQueue: Array<Buffer> = [];
+    fileQueue : Array < {
+        name: string,
+        data: Buffer
+    } > = [];
+
     s3 = new s3();
+
+    async streamToBuffer(stream): Promise < Buffer > {
+        return new Promise<Buffer>((resolve, reject) => {
+            const _buf: any[] = [];
+            stream.on('data', (chunk) => _buf.push(chunk));
+            stream.on('end', () => resolve(Buffer.concat(_buf)));
+            stream.on('error', (err) => reject(err));
+        });
+    }
 
     async readFiles() {
         const list = await this.s3.listObjects();
-        for(const i of list) {
-            if(i.Key.includes("PBM/ApproRx") && i.Key.includes("Detail")) {
-                
+        for await(const i of list) {
+            if (i.Key.includes("PBM/ApproRx") && i.Key.includes("Detail")) {
+                this.fileQueue.push({
+                    name: i.Key.split('/')[2].split('>')[1],
+                    data: await this.streamToBuffer(await this.s3.getFile(i.Key))
+                });
             }
         }
-    }
+}
 
-    async processFile(file: string) {
+async processFile(file : string) {
+    return new Promise(async (resolve, reject) => {
         const rows = await xlsxReader(`${tempFolder}/${file}`);
         for (let i = 1; i < rows.length; i++) {
             console.log(rows[i]);
         }
-    }
+        fs.unlink(`${tempFolder}/${file}`, () => {
+            resolve(true);
+        });
+    })
+}
 
-    async parseAll() {
-        // await this.processFile('sample.xlsx');
+async parseAll() {
+    try {
         await this.readFiles();
+        for await(const file of this.fileQueue) {
+            fs.writeFileSync(`${tempFolder}/${
+                file.name
+            }`, file.data);
+        }
+    
+
+    for await(const file of this.fileQueue) {
+        await this.processFile(file.name);
     }
+} catch (e) {
+
+}
+}
 }
 
 export default ApproRxDetailsParser;
