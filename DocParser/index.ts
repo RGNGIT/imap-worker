4d27322265a4ceeb10f6e4c5665a564a4ce29c77
RGNGIT <vnifedow@bk.ru>
2022-06-24 17:42:54 +0500

    +

--- DocParser/index.ts
+++ DocParser/index.ts
@@ -1,6 +1,13 @@
 import ApproRxDetailsParser from "./lib/approRxDetailsParser";
+import fs from 'fs';
 
 const start = async () => {
+    if(!fs.existsSync('./temp')) {
+        fs.mkdirSync('./temp');
+    } else {
+        fs.rmSync('./temp', {recursive: true});
+        fs.mkdirSync('./temp');
+    }
     await new ApproRxDetailsParser().parseAll();
 }
 
--- DocParser/lib/approRxDetailsParser.ts
+++ DocParser/lib/approRxDetailsParser.ts
@@ -2,21 +2,22 @@ import xlsxReader from 'read-excel-file/node';
 import fs from 'fs';
 import config from '../config';
 import s3 from '../s3';
+import { seq } from '../db';
 
-const {misc: {
-        tempFolder
-    }} = config;
+const { misc: {
+    tempFolder
+} } = config;
 
 class ApproRxDetailsParser {
 
-    fileQueue : Array < {
+    fileQueue: Array<{
         name: string,
         data: Buffer
-    } > = [];
+    }> = [];
 
     s3 = new s3();
 
-    async streamToBuffer(stream): Promise < Buffer > {
+    async streamToBuffer(stream): Promise<Buffer> {
         return new Promise<Buffer>((resolve, reject) => {
             const _buf: any[] = [];
             stream.on('data', (chunk) => _buf.push(chunk));
@@ -27,45 +28,34 @@ class ApproRxDetailsParser {
 
     async readFiles() {
         const list = await this.s3.listObjects();
-        for await(const i of list) {
+        for await (const i of list) {
             if (i.Key.includes("PBM/ApproRx") && i.Key.includes("Detail")) {
                 this.fileQueue.push({
-                    name: i.Key.split('/')[2].split('>')[1],
+                    name: i.Key.split('/')[2],
                     data: await this.streamToBuffer(await this.s3.getFile(i.Key))
                 });
             }
         }
-}
+    }
 
-async processFile(file : string) {
-    return new Promise(async (resolve, reject) => {
+    async processFile(file: string) {
         const rows = await xlsxReader(`${tempFolder}/${file}`);
         for (let i = 1; i < rows.length; i++) {
-            console.log(rows[i]);
+            await seq.query(`INSERT INTO pbm.detail 
+            (file_name, pbm_name, group_number, group_name, invc_number, invc_period_begin_date, invc_period_end_date, claim_no, original_claim_no, member_id, person_code, member_first_name, member_last_name, date_of_service, ndc_code, patient_paid, plan_paid, ingredient_code, dispensing_fee, sales_tax, other_fees, total_cost) VALUES 
+            ('${file}', 'ApproRx', 'gnum', 'gname', 'invcnum', 'ipbegin', 'ipend', '${rows[i][16]}', '${rows[i][17]}', 'memberid', '${rows[i][12]}', '${rows[i][14]}', '${rows[i][13]}', '${rows[i][23]}', 'ndccode', '${}');`);
         }
-        fs.unlink(`${tempFolder}/${file}`, () => {
-            resolve(true);
-        });
-    })
-}
-
-async parseAll() {
-    try {
-        await this.readFiles();
-        for await(const file of this.fileQueue) {
-            fs.writeFileSync(`${tempFolder}/${
-                file.name
-            }`, file.data);
-        }
-    
-
-    for await(const file of this.fileQueue) {
-        await this.processFile(file.name);
     }
-} catch (e) {
 
-}
-}
+    async parseAll() {
+        await this.readFiles();
+        for await (const file of this.fileQueue) {
+            fs.writeFileSync(`${tempFolder}/${file.name}`, file.data);
+        }
+        for await (const file of this.fileQueue) {
+            await this.processFile(file.name);
+        }
+    }
 }
 
 export default ApproRxDetailsParser;
