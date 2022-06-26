f1511babc12f00db2bc93be7360e043a19d9a570
RGNGIT <vnifedow@bk.ru>
2022-06-26 17:40:00 +0500

    f

--- DocParser/lib/approRxDetailsParser.ts
+++ DocParser/lib/approRxDetailsParser.ts
@@ -43,7 +43,7 @@ class ApproRxDetailsParser {
         for (let i = 1; i < rows.length; i++) {
             await seq.query(`INSERT INTO pbm.detail 
             (file_name, pbm_name, group_number, group_name, invc_number, invc_period_begin_date, invc_period_end_date, claim_no, original_claim_no, member_id, person_code, member_first_name, member_last_name, date_of_service, ndc_code, patient_paid, plan_paid, ingredient_code, dispensing_fee, sales_tax, other_fees, total_cost) VALUES 
-            ('${file}', 'ApproRx', 'gnum', 'gname', 'invcnum', 'ipbegin', 'ipend', '${rows[i][16]}', '${rows[i][17]}', 'memberid', '${rows[i][12]}', '${rows[i][14]}', '${rows[i][13]}', '${rows[i][23]}', 'ndccode', '${}');`);
+            ('${file.replace(/'/g, `''`)}', 'ApproRx', null, null, null, null, null, '${rows[i][16]}', '${rows[i][17]}', null, '${rows[i][12]}', '${rows[i][14].toString().replace(/'/g, `''`)}', '${rows[i][13].toString().replace(/'/g, `''`)}', '${rows[i][23]}', null, '${rows[i][30]}', ${rows[i][31]}, null, ${rows[i][28]}, null, null, null);`);
         }
     }
 
