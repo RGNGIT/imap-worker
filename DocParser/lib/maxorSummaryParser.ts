c67729a3b60bf7428179d28d2c4b7c03f8abe0ce
RGNGIT <vnifedow@bk.ru>
2022-07-08 22:39:49 +0500

    +

--- DocParser/lib/maxorSummaryParser.ts
+++ DocParser/lib/maxorSummaryParser.ts
@@ -12,22 +12,21 @@ const { misc: {
 const findValueByField = (textArray, fieldName, offset) => {
     const defaultOffset = 1;
     for(let i = 0; i < textArray.length; i++) {
-        if(textArray[i].text == fieldName || textArray[i].text.includes(fieldName)) {
-            return {value: textArray[i + defaultOffset + offset].text, index: i};
+        if(textArray[i].text == fieldName) {
+            return textArray[i + defaultOffset + offset].text;
         }
     }
     return null;
 }
 
-const indexUntil = (textArray, fromName, toName) => {
+const fetchOffset = (textArray, fromName, toName) => {
     let indexFrom = null, indexTo = null;
-    console.log(findValueByField(textArray, fromName, 0));
     for(let i = 0; i < textArray.length; i++) {
-        if(findValueByField(textArray, fromName, 0).value.includes(fromName)) {
-            indexFrom = findValueByField(textArray, fromName, 0).index;
+        if(textArray[i].text.includes(fromName)) {
+            indexFrom = i;
         }
-        if(findValueByField(textArray, toName, 0).value.includes(toName)) {
-            indexTo = findValueByField(textArray, toName, 0).index;
+        if(textArray[i].text.includes(toName)) {
+            indexTo = i;
         }
     }
     if(indexFrom && indexTo) {
@@ -78,8 +77,8 @@ class MaxorSummaryParser {
     async processAR01C(file, pages) {
         let i = 0;
         for await(const page of pages) {
-            const fromBillToName = indexUntil(page.texts, 'Bill To:', 'Billing Period');
-            // console.log(fromBillToName);
+            const fromBillToName = fetchOffset(page.texts, 'Bill To:', 'Billing Period');
+            console.log(fromBillToName);
             for await(const chunk of page.texts) {
                 // console.log(`INSERT INTO pbm.summary (file_name, pbm_name, group_number, group_name, invc_number, invc_period_begin_date, invc_period_end_date, rx_plan, rx_division, prescription_cost, fee_desc_1, fee_amt_1, fee_desc_2, fee_amt_2, fee_desc_3, fee_amt_3, fee_desc_4, fee_amt_4, fee_desc_5, fee_amt_5, total_invoice_amt) 
                 // VALUES ('${file.name}', 'Maxor', '');`);
